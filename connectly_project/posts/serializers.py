6d247136ae3490c85d9f761e185656cd8a894b44
jsc1989 <jsc1989@proton.me>
2025-01-25 01:36:14 +0800

    Fix CommentSerializer id references

--- connectly_project/posts/serializers.py
+++ connectly_project/posts/serializers.py
@@ -19,11 +19,11 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ['id', 'text', 'author', 'post', 'created_at']
 
     def validate_post(self, value):
-        if not Post.objects.filter(pk=value['post'].id).exists():
+        if not Post.objects.filter(id=value.id).exists():
             raise serializers.ValidationError("Post not found.")
         return value
     
     def validate_author(self, value):
-        if not User.objects.filter(pk=value['author'].id).exists():
+        if not User.objects.filter(id=value.id).exists():
             raise serializers.ValidationError("Author not found.")
         return value
